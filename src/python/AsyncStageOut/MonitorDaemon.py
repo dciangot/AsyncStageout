e7b3084fbd3de30afdb8cb5a562a95324aacea7f
dciangot <diego.ciangottini@cern.ch>
2016-02-22 17:08:49 +0100

    pyfts v0

---
+++ src/python/AsyncStageOut/MonitorDaemon.py
@@ -0,0 +1,121 @@
+#pylint: disable=C0103,W0105
+
+"""
+Here's the algorithm
+
+
+"""
+
+from WMCore.WorkerThreads.BaseWorkerThread import BaseWorkerThread
+from WMCore.WMFactory import WMFactory
+from AsyncStageOut.MonitorWorker import MonitorWorker
+
+import logging
+import os
+from multiprocessing import Pool
+
+result_list = []
+current_running = []
+
+def monitor( user, jobids, config):
+    """
+    Each worker executes this function.
+    """
+    logging.debug("Trying to start the worker")
+    try:
+        worker = MonitorWorker(user, jobids , config)
+    except Exception, ex:
+        logging.debug("Worker cannot be created!: %s" %ex)
+        return jobid
+    logging.debug("Worker created and init %s" % worker.init)
+    if worker.init:
+        logging.debug("Starting %s" %worker)
+        try:
+            worker()
+        except Exception, ex:
+            logging.debug("Worker cannot start!: %s" %ex)
+            return user
+    else:
+        logging.debug("Worker cannot be initialized!")
+    return user
+
+
+def log_result(result):
+    """
+    Each worker executes this callback.
+    """
+    result_list.append(result)
+    current_running.remove(result)
+
+class MonitorDaemon(BaseWorkerThread):
+    """
+    _TransferDaemon_
+    Call multiprocessing library to instantiate a MonitorWorker for each job.
+    """
+    def __init__(self, config):
+        """
+        Initialise class members
+        """
+        print "coasdokoaksd"
+        BaseWorkerThread.__init__(self)
+        # self.logger is set up by the BaseWorkerThread, we just set it's level
+        self.logger.info('Configuration loaded1')
+
+        self.config = config.Monitor
+        try:
+            self.logger.setLevel(self.config.log_level)
+        except ():
+            import self.logger
+            self.logger = self.logger.getLogger()
+            self.logger.setLevel(self.config.log_level)
+
+        self.logger.debug('Configuration loaded')
+        self.dropbox_dir = '%s/dropbox/outputs' % self.config.componentDir
+        try:
+            os.path.isdir(self.dropbox_dir)
+        except():
+            self.logger.error('dropbox dir not foud')
+            raise
+        self.pool = Pool(processes=self.config.pool_size)
+        self.factory = WMFactory(self.config.schedAlgoDir, namespace = self.config.schedAlgoDir)
+
+
+    def algorithm(self, parameters = None):
+        """
+
+        """
+        files = os.listdir('/data/srv/asyncstageout/v1.0.4/install/asyncstageout/AsyncTransfer/dropbox/outputs/')
+
+        size = len(files)
+
+        if size <= self.config.pool_size:
+            users = files
+        else:
+            sorted_jobs = self.factory.loadObject(self.config.algoName,
+                                                  args = [self.config, self.logger, files, self.config.pool_size],
+                                                  getFromCache = False, listFlag = True)
+            users = sorted_jobs()[:self.config.pool_size]
+
+	
+        self.logger.debug('Number of users in monitor: %s' % len(current_running))
+
+	for user in users:
+	 files = os.listdir('/data/srv/asyncstageout/v1.0.4/install/asyncstageout/AsyncTransfer/dropbox/outputs/%s'%user)
+	 if len(files)>0: 
+	  def keys_map(inputDict):
+                """
+                Map function.
+                """
+                return inputDict.split(".")[1]
+	  jobs = map(keys_map, files)
+          if user not in current_running:
+                self.logger.debug('monitoring job IDs: %s' % jobs)
+                current_running.append(user)
+                self.pool.apply_async(monitor,(user,jobs, self.config), callback = log_result)
+
+    def terminate(self, parameters = None):
+        """
+        Called when thread is being terminated.
+        """
+        self.pool.close()
+        self.pool.join()
---
+++ src/python/AsyncStageOut/MonitorWorker.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env
+'''
+The MonitorWorker does the following:
+
+'''
+import re
+import time
+import logging
+import subprocess
+import os
+import datetime
+import traceback
+import json
+from time import strftime
+from AsyncStageOut import getHashLfn
+from AsyncStageOut import getDNFromUserName
+from WMCore.Credential.Proxy import Proxy
+
+def getProxy(userdn, group, role, defaultDelegation, logger):
+    """
+    _getProxy_
+    """
+
+    logger.debug("Retrieving proxy for %s" % userdn)
+    config = defaultDelegation
+    config['userDN'] = userdn
+    config['group'] = group
+    config['role'] = role
+    proxy = Proxy(defaultDelegation)
+    proxyPath = proxy.getProxyFilename( True )
+    timeleft = proxy.getTimeLeft( proxyPath )
+    if timeleft is not None and timeleft > 3600:
+        return (True, proxyPath)
+    proxyPath = proxy.logonRenewMyProxy()
+    timeleft = proxy.getTimeLeft( proxyPath )
+    if timeleft is not None and timeleft > 0:
+        return (True, proxyPath)
+    return (False, None)
+
+def execute_command(command, logger, timeout):
+    """
+    _execute_command_
+    Funtion to manage commands.
+    """
+    stdout, stderr, rc = None, None, 99999
+    proc = subprocess.Popen(command, shell=True, cwd=os.environ['PWD'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE,)
+    t_beginning = time.time()
+    while True:
+        if proc.poll() is not None:
+            break
+        seconds_passed = time.time() - t_beginning
+        if timeout and seconds_passed > timeout:
+            proc.terminate()
+            logger.error('Timeout in %s execution.' % command)
+            return stdout, rc
+        time.sleep(0.1)
+    stdout, stderr = proc.communicate()
+    rc = proc.returncode
+    logger.debug('Executing : \n command : %s\n output : %s\n error: %s\n retcode : %s' % (command, stdout, stderr, rc))
+    return stdout, rc
+
+
+class MonitorWorker:
+
+    def __init__(self,user, job, config):
+        """
+        store the user and tfc the worker
+        """
+        self.jobids = job 
+        self.config = config
+        self.logger = logging.getLogger('MonitorTransfer-Worker-%s' % user)
+        self.commandTimeout = 1200
+        self.uiSetupScript = getattr(self.config, 'UISetupScript', None)
+        self.init = True
+        if getattr(self.config, 'cleanEnvironment', False):
+            self.cleanEnvironment = 'unset LD_LIBRARY_PATH; unset X509_USER_CERT; unset X509_USER_KEY;'
+        # TODO: improve how the worker gets a log
+
+
+        self.user = user
+        self.logger.debug("Trying to get DN for %s" %self.user)
+        try:
+            self.userDN = getDNFromUserName(self.user, self.logger)
+        except Exception, ex:
+            msg = "Error retrieving the user DN"
+            msg += str(ex)
+            msg += str(traceback.format_exc())
+            self.logger.error(msg)
+            self.init = False
+            return
+        if not self.userDN:
+            self.init = False
+            return
+        defaultDelegation = {
+                                  'logger': self.logger,
+                                  'credServerPath' :  self.config.credentialDir,
+                                  # It will be moved to be getfrom couchDB
+                                  'myProxySvr': 'myproxy.cern.ch',
+                                  'min_time_left' : getattr(self.config, 'minTimeLeft', 36000),
+                                  'serverDN' : self.config.serverDN,
+                                  'uisource' : self.uiSetupScript,
+                                  'cleanEnvironment' : getattr(self.config, 'cleanEnvironment', False)
+                            }
+        if hasattr(self.config, "cache_area"):
+            try:
+                defaultDelegation['myproxyAccount'] = re.compile('https?://([^/]*)/.*').findall(self.config.cache_area)[0]
+            except IndexError:
+                self.logger.error('MyproxyAccount parameter cannot be retrieved from %s' % self.config.cache_area)
+                pass
+        if getattr(self.config, 'serviceCert', None):
+                defaultDelegation['server_cert'] = self.config.serviceCert
+        if getattr(self.config, 'serviceKey', None):
+                defaultDelegation['server_key'] = self.config.serviceKey
+
+        self.logger.debug('cache: %s' %self.config.cache_area)
+        self.valid = False
+        try:
+
+            self.valid, proxy = getProxy(self.userDN, "", "", defaultDelegation, self.logger)
+
+        except Exception, ex:
+
+            msg = "Error getting the user proxy"
+            msg += str(ex)
+            msg += str(traceback.format_exc())
+            self.logger.error(msg)
+
+        if self.valid:
+            self.userProxy = proxy
+        else:
+            # Use the operator's proxy when the user proxy in invalid.
+            # This will be moved soon
+            self.logger.error('Did not get valid proxy. Setting proxy to ops proxy')
+            self.userProxy = config.opsProxy
+
+        # Proxy management in Couch
+        os.environ['X509_USER_PROXY'] = self.userProxy
+
+    def __call__(self):
+        """
+        """
+        success = False
+	while not success:
+	 success = True
+	 for jid in self.jobids:
+	    self.jobid=jid
+            command = "glite-transfer-status -l -s %s %s" \
+                    % (self.config.fts_server, self.jobid)
+            result, rc = execute_command(command, self.logger, self.commandTimeout)
+            self.logger.info("job %s status: %s" % (self.jobid, result.split("\n")[0]))
+            if "Finished" in result.split("\n")[0] or result.split("\n")[0]=="Failed":
+		self.jobids.remove(jid)
+                reporter = {
+                        "LFNs": [],
+                        "transferStatus": [],
+                        "failure_reason": [],
+                        "timestamp": [],
+                        "username": ""
+                }
+                lfns = []
+                statuses = []
+                reasons = []
+                timestamps = []
+                user = ''
+
+                for source_ in result.split("\n"):
+                    if "Source:" in source_:
+                        source = "/store/temp/user/%s" % source_.split(":      ")[1].split("/user/")[1]
+                        index = result.split("\n").index(source_)
+                        state = result.split("\n")[index+2].split(":       ")[1]
+                        reason = result.split("\n")[index+4].split(":      ")[1]
+                        user = source_.split(":      ")[1].split("/user/")[1].split(".")[0]
+                        lfns.append(source)
+                        statuses.append(state)
+                        reasons.append(reason)
+                        timestamps.append('')
+                        self.logger.debug("ASO doc %s of user %s in state %s" % (getHashLfn(source), user, state))
+
+                reporter["LFNs"] = lfns
+                reporter["transferStatus"] = statuses
+                reporter["username"] = user
+                reporter["reasons"] = reasons
+                reporter["timestamp"] = timestamps
+
+                report_j = json.dumps(reporter)
+
+                self.logger.debug("Creating report %s" % report_j)
+                try:
+                    if not os.path.exists(self.config.componentDir+"/work/%s" %user):
+                        os.makedirs(self.config.componentDir+"/work/%s" %user)
+                    out_file = open(self.config.componentDir+"/work/%s/Reporter.%s.json"%(user,self.jobid),"w")
+                    out_file.write(report_j)
+                    out_file.close()
+		    os.remove('/data/srv/asyncstageout/v1.0.4/install/asyncstageout/AsyncTransfer/dropbox/outputs/%s/Monitor.%s.json'%(user,self.jobid))
+                except Exception as ex:
+                    msg="Cannot create fts job report: %s" %ex
+                    self.logger.error(msg)
+		
+	    else:
+		success = False	
+         time.sleep(self.config.job_poll_intervall)
+        return
